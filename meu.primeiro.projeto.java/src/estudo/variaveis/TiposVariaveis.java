769de36498ee89acc944bd6eefbca852b9074c9e
Laura <laurabgularte@gmail.com>
2024-11-15 14:58:15 -0300

    add types of variables examples

---
+++ meu.primeiro.projeto.java/src/estudo/variaveis/TiposVariaveis.java
@@ -0,0 +1,23 @@
+package estudo.variaveis;
+public class TiposVariaveis {
+  public static void main(String[] args) throws Exception {
+    //tipos primitivos 
+    double salarioMinimo = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+     
+  
+    int numero = 1;
+    numero = 2;     //mudanca do valor da variavel 
+    
+    final double VALOR_DE_PI = 3.14; // variavel com 'final' na frente e em caixa alta nao muda seu valor
+    
+
+    // classe string representa texto na aplicacao
+     
+    String meuNome = "Laura Gularte";
+  }
+
+}
