fadf6a10f03c207fc0de72d280fb20d6d0a2f615
Laura <laurabgularte@gmail.com>
2024-11-16 13:59:42 -0300

    add file about java operators

---
+++ meu.primeiro.projeto.java/src/estudo/operadores/ClassificacaoOperadores.java
@@ -0,0 +1,94 @@
+package estudo.operadores;
+
+import java.sql.Date;
+
+// operadores de atribuição
+// representados pelo simbolo =
+public class ClassificacaoOperadores {
+  public static void main(String[] args){
+  String nome = "Laura";
+  int idade = 22;
+  double peso = 58.5;
+  char sexo = 'F';
+  boolean doadorOrgao = false;
+  Date dataNascimento = new Date(0);
+
+  // operadores aritmeticos
+
+double soma = 10.5 + 15.7;
+int subtração = 113 - 25;
+int multiplicacao = 20 * 7;
+int divisao = 15 / 3;
+int modulo = 18 % 3;
+double resultado = (10 * 7) + (20/4);
+
+// ATENÇÃO! O operador de adição (+), quando utilizado em variáveis do tipo texto, realizará a “concatenação de textos”.
+
+// operadores unários 
+
+int numero = 5;
+		
+//Imprimindo o numero negativo
+System.out.println(- numero);
+
+//incrementando numero em mais 1 numero, imprime 6
+numero ++;
+System.out.println(numero);
+
+//incrementando numero em mais 1 numero, imprime 7
+System.out.println(numero ++);// ops algo de errado não está certo
+
+System.out.println(numero);// agora sim, numero virou 7
+
+//ordem de precedencia conta aqui
+System.out.println(++ numero);
+
+boolean verdadeiro = true;
+
+System.out.println("Inverteu " + !verdadeiro);
+
+
+// operadores relacionais 
+
+int numero1 = 1;
+int numero2 = 2;
+
+if(numero1 > numero2)
+	System.out.print("Numero 1 maior que numero 2");
+
+if(numero1 < numero2)
+	System.out.print("Numero 1 menor que numero 2");
+
+if(numero1 >= numero2)
+	System.out.print("Numero 1 maior ou igual que numero 2");
+
+if(numero1 <= numero2)
+	System.out.print("Numero 1 menor ou igual que numero 2");
+
+if(numero1 != numero2)
+	System.out.print("Numero 1 é diferente de numero 2");
+  }
+
+  //operadores logicos
+
+boolean condicao1=true;
+
+boolean condicao2=false;{
+/* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+expressões. 
+É como se estivesse escrito:
+ "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+*/
+
+if(condicao1 && condicao2)
+	System.out.print("Os dois valores precisam ser verdadeiros");
+
+//Se condicao1 OU condicao2 for verdadeira, executar código.
+if(condicao1 || condicao2)
+	System.out.print("Um dos valores precisa ser verdadeiro");
+}
+
+
+}
+
+
