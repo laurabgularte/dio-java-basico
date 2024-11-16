b62e35101d2bed89079f7568f08510c084726c39
Laura <laurabgularte@gmail.com>
2024-11-16 14:49:32 -0300

    add files on javadocs

---
+++ meu.primeiro.projeto.java/src/estudo/documentacao/Comentarios.java
@@ -0,0 +1,30 @@
+package estudo.documentacao;
+
+public class Comentarios {
+  public static void main(String[] args) {
+    // Olá, eu sou um comentário em uma única linha
+
+
+    /* Olá,
+ * Eu sou um comentario
+ * que posso ser mais detalhadod
+ * quando necessário
+ */
+
+
+ /** 
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
+
+
+//JAVADOCS - Criando nossa documentação no formato html para disponibilizar via web.
+
+ // No terminal execute o comando abaixo
+
+// javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
+  }
+}
