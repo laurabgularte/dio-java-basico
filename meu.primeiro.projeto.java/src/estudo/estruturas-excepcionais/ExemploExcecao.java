2d7f76247ab625aa0a85ab69ac8a9044c35784f6
Laura <laurabgularte@gmail.com>
2024-11-18 23:28:07 -0300

    add file on java exceptions

---
+++ meu.primeiro.projeto.java/src/estudo/estruturas-excepcionais/ExemploExcecao.java
@@ -0,0 +1,15 @@
+public class ExemploExcecao {
+
+  public static void main(String[] args) {
+    try {
+      //  bloco de código conforme esperado
+    }
+    catch(Exception e) {// precisamos saber qual exceção
+      // bloco de código que captura as exceções que podem acontecer
+      // em caso de um fluxo não previsto
+    }
+
+    //A instrução finally permite definir um bloco de código a ser executado independente de ocorrer um erro ou não.
+  }
+}
+
